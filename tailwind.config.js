e51737cc55e250f0642a4d20097e142124bbe9c7
Sahariar Kabir <sronok@gmail.com>
2022-12-25 01:22:29 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#13a538",
+        
+"secondary": "#f39100",
+        
+"accent": "#954a97",
+        
+"neutral": "#1f2937",
+        
+"base-100": "#f5f5f4",
+        
+"info": "#1D49E7",
+        
+"success": "#28B8A5",
+        
+"warning": "#ffcc00",
+        
+"error": "#F55C85",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
