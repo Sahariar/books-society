01c475bc621670a4e41d7124b1097e9210e2d86a
Md Rubel Rana <96787688+rubelrana123@users.noreply.github.com>
2022-12-26 09:24:24 +0600

    added brandarea section

---
+++ components/HomePage/BrandArea.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const BrandArea = () => {
+  return (
+    <div className=' grid lg:grid-cols-6 md:grid-cols-3 grid-cols-2 gap-5'>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_1.svg" alt="" />
+      </div>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_2.svg" alt="" />
+      </div>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_3.svg" alt="" />
+      </div>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_4.svg" alt="" />
+      </div>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_5.svg" alt="" />
+      </div>
+      <div>
+         <img src="https://demo2.pavothemes.com/bookory/wp-content/uploads/2022/02/brand_6.svg" alt="" />
+      </div>
+ 
+    </div>
+  );
+};
+
+export default BrandArea;
