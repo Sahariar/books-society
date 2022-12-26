7b6b740410366027d2e5c7b3b28f9258b851718b
Sahariar Kabir <sronok@gmail.com>
2022-12-26 10:26:52 +0600

    update bookscategories

---
+++ components/HomePage/BestSellers.js
@@ -0,0 +1,15 @@
+
+
+const BestSellers = () => {
+    return (
+        <section className="books-selling-section my-10">
+            <div className="p-12 my-10 container mx-auto">
+			<h2 className="text-4xl font-bold text-center pt-5">
+                Best Selling Books
+			</h2>
+			</div>
+        </section>
+    );
+};
+
+export default BestSellers;
