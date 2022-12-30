400b0fe487a7474f0b71b8fb2656d066082f51fd
S. M. Sujan <smsujon50@gmail.com>
2022-12-31 00:16:10 +0600

    Dashboard pages created

---
+++ components/Dashboard/Chat/Chat.js
@@ -0,0 +1,11 @@
+
+const Chat = () => {
+    return (
+        <div className="p-6 grid grid-cols-5 gap-6">
+            <div className="col-span-2 bg-[#FAFAFB] p-5 rounded-2xl">Hello</div>
+            <div className="col-span-3 bg-[#FAFAFB] p-5 rounded-2xl">Hello</div>
+        </div>
+    );
+};
+
+export default Chat;
